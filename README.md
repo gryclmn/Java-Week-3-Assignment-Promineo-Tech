f8dc9b37cb9a40a8283f973eed2d28ad4edd16b1
Gary Coleman <gryclmn@hotmail.com>
2019-08-08 05:18:34 -0700

    Finished assignment. Added README with assignment instructions.

---
+++ README.md
@@ -0,0 +1,29 @@
+# Week-3-Assignment-Promineo-Tech
+
+Instructions: In Eclipse, or an IDE of your choice, write the code that accomplishes the objectives listed below. 
+Ensure that the code compiles and runs as directed. Take screenshots of the code and of the running program 
+(make sure to get screenshots of all required functionality) and paste them in this document where instructed below. 
+Create a new repository on GitHub for this week�s assignments and push this document, with your Java project code, 
+to the repository. Add the URL for this week�s repository to this document where instructed and submit this document 
+to your instructor when complete.
+
+Coding Steps:
+1.	Create an array of int called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93.
+    a.	Programmatically subtract the value of the first element in the array from the value in the last element of the array (do not type ANY numbers in the operation, ages[7] – ages[0] is not allowed). Print the result to the console.
+    b.	Add a new age to your array and repeat the step above to ensure it is dynamic (works for arrays of different lengths).
+    c.	Use a loop to iterate through the array and calculate the average age. Print the result to the console.
+2.	Create an array of String called names that contains the following values: “Sam”, “Tommy”, “Tim”, “Sally”, “Buck”, “Bob”.
+    a.	Use a loop to iterate through the array and calculate the average number of letters per name. Print the result to the console.
+    b.	Use a loop to iterate through the array again and concatenate all the names together, separated by spaces, and print the result to the console.
+3.	How do you access the last element of any array?
+4.	How do you access the first element of any array?
+5.	Create a new array of int called nameLengths. Write a loop to iterate over the previously created names array and add the length of each name to the nameLengths array.
+6.	Write a loop to iterate over the nameLengths array and calculate the sum of all the elements in the array. Print the result to the console.
+7.	Write a method that takes a String, word, and an int, n, as arguments and returns the word concatenated to itself n number of times. (i.e. if I pass in “Hello” and 3, I would expect the method to return “HelloHelloHello”).
+8.	Write a method that takes two Strings, firstName and lastName, and returns a full name (the full name should be the first and the last name as a String separated by a space).
+9.	Write a method that takes an array of int and returns true if the sum of all the ints in the array is greater than 100.
+10.	Write a method that takes an array of double and returns the average of all the elements in the array.
+11.	Write a method that takes two arrays of double and returns true if the average of the elements in the first array is greater than the average of the elements in the second array.
+12.	Write a method called willBuyDrink that takes a boolean isHotOutside, and a double moneyInPocket, and returns true if it is hot outside and if moneyInPocket is greater than 10.50.
+13.	Create a method of your own that solves a problem. In comments, write what the method does and why you created it.
+
---
+++ src/App.java
@@ -0,0 +1,193 @@
+import java.time.Year;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+import javax.management.loading.PrivateClassLoader;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.IconifyAction;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		/* 
+		 * ********************
+		 * Step 1
+		 * ********************
+		 */
+		
+		int[] ages = {3, 9, 23, 64, 2, 8, 28};
+		
+		// Instructions are to get the difference of the first and last element.
+		// However we are not allowed to use any numbers at all...
+		int lengthOfArr = ages.length;
+		int difference = Math.abs(ages[lengthOfArr - lengthOfArr] - ages[lengthOfArr - 1]);
+		
+		System.out.println("Difference of first and last element in ages[] = " + difference);
+		
+		int[] newAges = new int[ages.length];
+		
+		System.arraycopy(ages,  0, newAges, 0, ages.length);
+		
+		lengthOfArr = newAges.length;
+		newAges[lengthOfArr - 1] = 100;
+		difference = Math.abs(newAges[lengthOfArr - lengthOfArr] - newAges[lengthOfArr - 1]);
+		
+		System.out.println("Difference of first and last element in newAges[] = " + difference);
+		
+		int sum = 0;
+		for (int i = 0; i < newAges.length; i++) {
+			sum += newAges[i];
+		}
+		double average = sum / lengthOfArr;
+		System.out.println("The average age in newAges[] = " + average );
+		
+		/* 
+		 * ********************
+		 * Step 2
+		 * ********************
+		 */
+		
+		String[] names = {"Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"};
+		sum = 0;
+		for (int i = 0; i < names.length; i++) {
+			sum += names[i].length();
+		}
+		average = sum / names.length;
+		System.out.println("The average number of letters per name in names[] = " + average );
+		
+		String allNames = "";
+		for (int i = 0; i < names.length; i++) {
+			allNames += names[i] + " ";
+		}
+		System.out.println(allNames);
+		
+		/* 
+		 * ********************
+		 * Step 3
+		 * ********************
+		 */
+		
+		System.out.println("Use array[arr.length] to access the last element of an array");
+		
+		/* 
+		 * ********************
+		 * Step 4
+		 * ********************
+		 */
+		
+		System.out.println("Use array[0] to access the first element of an array");
+		
+		/* 
+		 * ********************
+		 * Step 5
+		 * ********************
+		 */
+		
+		int[] nameLengths = new int[names.length];
+		for (int i = 0; i < names.length; i++) {
+			nameLengths[i] = names[i].length(); 
+		}
+		
+		/* 
+		 * ********************
+		 * Step 6
+		 * ********************
+		 */
+		
+		sum = 0;
+		for (int i = 0; i < nameLengths.length; i++) {
+			sum += nameLengths[i];
+		}
+		System.out.println("The sum of all lengths in nameLengths[] = " + sum);
+		
+		/* 
+		 * ********************
+		 * Step 7
+		 * ********************
+		 */
+		
+		System.out.println(duplicateWord("Hello", 5));
+		
+		/* 
+		 * ********************
+		 * Step 8
+		 * ********************
+		 */
+		
+		System.out.println(getFullName("Bruce", "Banner"));
+		
+		/* 
+		 * ********************
+		 * Step 9
+		 * ********************
+		 */
+		
+		System.out.println("checkIfSumGreaterThan100() returns => " + checkIfSumGreaterThan100(ages));
+		
+		/* 
+		 * ********************
+		 * Step 10
+		 * ********************
+		 */
+		
+		double[] doubleArr = {0.1, 1.0, 236.4592, 1000.1};
+		double[] emptyDoubleArr = {};
+		System.out.println("getAverageOfDoubleArr() returns => " + getAverageOfDoubleArr(doubleArr));
+		System.out.println("Passing empty double[] to getAverageOfDoubleArr() returns => " + getAverageOfDoubleArr(emptyDoubleArr));
+	
+		/* 
+		 * ********************
+		 * Step 11
+		 * ********************
+		 */
+		
+		double[] anotherDoubleArr = {2.2, 3.3, 4.4, 5.5};
+		System.out.println("isFirstArrAvgGreater returns => " + isFirstArrAvgGreater(doubleArr, anotherDoubleArr));
+		
+		/* 
+		 * ********************
+		 * Step 12
+		 * ********************
+		 */
+		System.out.println("getAge() returns => " + getAge(1981));
+		
+		
+		for (int i = 0; i < 100; i++) {
+			if (i % 2 != 0) System.out.println(i);
+		}
+	}
+	
+	private static String duplicateWord(String word, int n) {
+		String duplicatedWord = "";
+		for (int i = 0; i < n; i++) {
+			duplicatedWord += word;
+		}
+		return "duplicatedWord() returns => " + duplicatedWord;
+	}
+	
+	private static String getFullName(String firstName, String lastName) {
+		return "getFullName() returns => " + firstName + " " + lastName;
+	}
+	
+	private static Boolean checkIfSumGreaterThan100(int[] arr) {
+		return Arrays.stream(arr).sum() > 100;
+	}
+	
+	private static double getAverageOfDoubleArr(double[] arr) {
+		// We need to use .orElse() in case an empty array is passed in
+		return Arrays.stream(arr).average().orElse(Double.NaN);
+	}
+	
+	private static boolean isFirstArrAvgGreater(double[] firstArr, double[] secondArr) {
+		return getAverageOfDoubleArr(firstArr) > getAverageOfDoubleArr(secondArr); 
+	}
+	
+	
+	/*
+	 * getAge calculates a new user's age with the given birthYear
+	 */
+	private static int getAge(int birthYear) {
+		return Year.now().getValue() - birthYear; 
+	}
+
+}
